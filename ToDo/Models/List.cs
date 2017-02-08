f54b54b1148a066dcc3dcec4def03ec0270ac3f5
Austen Perelman-Hall <austenph@gmail.com>
2017-02-08 11:40:38 -0500

    created List and Task Models

---
+++ ToDo/Models/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class List
+    {
+        [Key]
+        public int listID { get; set;}
+
+        public string Title { get; set; }
+        DateTime Date { get; set; }
+         
+    }
+}
